dc33fe33bb312c7b072d5edd3f81aebb27767130
stanislasvallet <stanislasvallet@live.com>
2019-11-08 17:13:40 +0100

    TP09 : initialisation fight

--- TP09/js/arena.js
+++ TP09/js/arena.js
@@ -17,6 +17,29 @@ console.log("job = "+job);
 initCharacter();
 initCharacter("Richar Aladana","Boxer")
 
+
+let titleElement = document.querySelector("h2");
+titleElement.innerText = "Welcome to "+pseudo+" the "+ job;
+
+let initFight = function(){
+    let startingLifePointsInput = document.getElementById("startingLifePoints");
+
+    let currentFight = {
+        startingLifePoints: startingLifePointsInput.value
+    };
+
+    if(currentFight.startingLifePoints > 0){
+        console.log("Starting life points  = "+ currentFight.startingLifePoints);
+        document.getElementById("userLifePoints").innerText = currentFight.startingLifePoints;
+        document.getElementById("aiLifePoints").innerText = currentFight.startingLifePoints;
+
+        currentFight.aiActionsArray = [];
+    } else {
+        console.error("erreur, life input is negative");
+    }
+}
+
+
 window.onload = function () { // Faire en sorte que le html soit complétement chargé
     //Code for events will be here !
     let beginFightButton = document.getElementById("beginFight");
